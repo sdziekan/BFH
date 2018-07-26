dde6569bfddc049e6ff3f152ee8277089cb0b635
Salvatore Dziekan <sdziekan@gmail.com>
2018-07-26 17:15:17 -0400

    started populating the web pages with data from prior version of app

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,18 +1,60 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app>
+      <v-navigation-drawer v-model="drawer" app right stateless>
+        <v-list>
+          <v-expansion-panel app>
+            <v-expansion-panel-content v-for="(item,i) in menu" :key="i">
+              <div slot="header"> {{ item.header }}</div>
+              <v-card>
+                <v-card-text v-text="item.message"></v-card-text>
+              </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app fixed color="indigo lighten-5" class="elevation-6" height="100px">
+
+        <v-toolbar-title v-text="toolbar_title"></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid justify-center align-center>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+
+      <v-footer app inset fixed>
+        <div class="pa-3">2018</div>
+        <v-spacer></v-spacer>
+        <div class="pa-3">BETA</div>
+      </v-footer>
+
+    </v-app>
+
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+<script>
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+
+  data: () => ({
+    toolbar_title: "Tim Ferriss Show Book Recommendations",
+    drawer: null,
+    props: {
+      source: String
+    },
+    menu: [
+      { header: 'About', message: 'As an amateur developer, Tim Ferriss fan and book lover, I built this site to create a comprehensive database of book recommendations from the Tim Ferriss Show podcast. I am in no way affiliated with Tim Ferriss or the Tim Ferriss Show.' },
+      { header: 'Contact', message: 'Please let me know what you think at sdziekan@gmail.com or @samsthesis on Twitter.' },
+      { header: 'Disclaimer', message: 'I have placed Amazon Affiliate links on this site to help offset the cost of building and maintaining it. ' }
+    ]
+  })
+
 }
 </script>
 
--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
